e3c0fc4ceeec267f6754a88a0607dd89b666feab
harvey magana <heyharv2012@gmail.com>
2020-10-18 12:48:24 -0700

    Removed props from the Products component and no more errors are thrown from this component.

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,17 +4,16 @@ import ProductContext from '../contexts/ProductContext';
 // Components
 import Product from './Product';
 
-const Products = props => {
-	console.log(props)
-	const { products } = useContext(ProductContext);
-	
+const Products = () => {
+	const { products, addItem } = useContext(ProductContext);
+
 	return (
 		<div className="products-container">
-			{props.products.map(product => (
+			{products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+					addItem={addItem}
 				/>
 			))}
 		</div>
